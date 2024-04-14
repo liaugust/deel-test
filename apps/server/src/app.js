c2611872718e571f9c0672f0a8b0c1a53689ebc8
Ivan Lipatov <ivan.liaugust@gmail.com>
2024-04-14 23:35:19 +0300

    feat(server): add profiles endpoint

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -19,9 +19,44 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
 	const { Contract } = req.app.get('models')
 	const { id } = req.params
 	const contract = await Contract.findOne({ where: { id } })
+
 	if (!contract) {
 		return res.status(HTTP_STATUS.NOT_FOUND).end()
 	}
+
 	res.json(contract)
 })
 
+// Returns a list of profiles, useful for your login
+app.get("/profiles", async (req, res) => {
+	const { Profile } = req.app.get("models");
+	const { type } = req.query;
+
+	if (type === "client") {
+		const profiles = await Profile.findAll({
+			where: {
+				type: "client",
+			},
+		});
+
+		return res.json(profiles);
+	}
+
+	if (type === "contractor") {
+		const profiles = await Profile.findAll({
+			where: {
+				type: "contractor",
+			},
+		});
+
+		return res.json(profiles);
+	}
+
+	const profiles = await Profile.findAll();
+
+	res.json(profiles);
+});
+
+app.get("/profile", getProfile, async (req, res) => {
+	res.json(req.profile);
+});
