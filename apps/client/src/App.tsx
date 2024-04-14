d89a423a90d1311347ac447e380ad04632ccfe67
Ivan Lipatov <ivan.liaugust@gmail.com>
2024-04-15 00:20:10 +0300

    feat(server): add "deposit" endpoint

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,8 +1,6 @@
 import {Header} from "@/components/header/header.tsx";
 
 function App() {
-	
-	
 	return (
 		<div className='max-w-[1280px] w-full m-auto p-4'>
 			<Header />
--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -66,3 +66,56 @@ app.get('/profiles', async (req, res) => {
 app.get('/profile', getProfile, async (req, res) => {
 	res.json(req.profile);
 });
+
+// FIXME needs clarification.
+// Why not to just deposit by current profile id?
+// app.post("/balances/deposit/:userId", getProfile, async (req, res) => {
+app.post('/balances/deposit', getProfile, async (req, res) => {
+	const { Profile, Job, Contract } = req.app.get('models');
+	const { amount } = req.body;
+	const { profile } = req;
+
+	console.log('Profile before deposit', profile);
+
+	if (profile.type !== 'client') {
+		return res.status(HTTP_STATUS.FORBIDDEN).json({ error: 'Only clients can deposit money' });
+	}
+
+	// FIXME needs clarification.
+	// The requirement says "a client can't deposit more than 25% his total of jobs to pay".
+	//
+	// const activeContracts = await Contract.findAll({
+	//  where: {
+	// 	 status: 'in_progress',
+	// 	 ClientId: profile.id,
+	//  },
+	//  include: [
+	// 	 {
+	// 		 model: Job,
+	// 		 as: 'Jobs',
+	// 		 where: {
+	// 		  paid: null,
+	// 		 },
+	// 	 },
+	//  ],
+	// });
+	//
+	// const jobs = activeContracts.flatMap((contract) => contract.Jobs);
+	//
+	// const total = jobs.reduce((acc, job) => acc + job.price, 0);
+	// const quarterOfTotal = total / 4;
+	// if (amount > quarterOfTotal) {
+	//    console.log("amount > quarterOfTotal");
+	//    return res.status(HTTP_STATUS.BAD_REQUEST).json({
+	//      error: "Deposit amount exceeds the quarter of the total job price",
+	//    });
+	// }
+
+	await Profile.increment('balance', {
+		by: amount,
+		where: { id: profile.id },
+		returning: true,
+	});
+
+	res.status(HTTP_STATUS.OK).end();
+});
