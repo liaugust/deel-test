255ff9ce9395bd3f422172a3af1790567604df43
Ivan Lipatov <ivan.liaugust@gmail.com>
2024-04-14 23:28:23 +0300

    refactor(server): add http status constant

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model')
+const { getProfile } = require('./middleware/getProfile')
+const { HTTP_STATUS } = require('./utils/constants');
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
@@ -11,11 +12,14 @@ app.set('models', sequelize.models)
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
+app.get('/contracts/:id', getProfile, async (req, res) => {
+	const { Contract } = req.app.get('models')
+	const { id } = req.params
+	const contract = await Contract.findOne({ where: { id } })
+	if (!contract) {
+		return res.status(HTTP_STATUS.NOT_FOUND).end()
+	}
+	res.json(contract)
 })
+
 module.exports = app;
--- apps/server/src/middleware/getProfile.js
+++ apps/server/src/middleware/getProfile.js
@@ -1,8 +1,13 @@
+const { HTTP_STATUS } = require('../utils/constants');
 
 const getProfile = async (req, res, next) => {
     const {Profile} = req.app.get('models')
     const profile = await Profile.findOne({where: {id: req.get('profile_id') || 0}})
-    if(!profile) return res.status(401).end()
+
+    if(!profile) {
+        return res.status(HTTP_STATUS.UNAUTHORIZED).end()
+    }
+
     req.profile = profile
     next()
 }
